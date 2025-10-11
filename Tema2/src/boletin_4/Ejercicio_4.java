f9f5fd874062d5b2d36732c9efc007dedefbd233
juanBueno-coder <juanbueno-coder@gmail.com>
2025-10-11 20:58:38 +0200

    ejercicio 4

--- Tema2/src/boletin_4/Ejercicio_4.java
+++ Tema2/src/boletin_4/Ejercicio_4.java
@@ -10,8 +10,18 @@ public class Ejercicio_4 {
 		int provincia = 0;
 		String nombreProvincia = "";
 		
-		numeroVotosA=0;
-		numeroVotosB=0;
+		int numeroVotosA = 0;
+		int numeroVotosB = 0;
+		int abstenciones = 0;
+		int votosTotales = 0;
+		
+		String ProvinciaAFavorita = "";
+		String ProvinciaBFavorita = "";
+		
+		int mayorVotosB = Integer.MIN_VALUE;
+		int mayorVotosA = Integer.MIN_VALUE;
+		
+		boolean altaAbstencion =false;
 		
 		switch (numRegion) {
 		case 1:
@@ -27,15 +37,37 @@ public class Ejercicio_4 {
 		default:System.out.println("No has introducido un numero valido.");
 			break;
 		}
-		for (int i = 1; i < provincia; i++) {
+		for (int i = 1; i <= provincia; i++) {
+			
 			System.out.println("Dame el nombre de la provincia");
 			nombreProvincia = sc.next();
+			
+			
 			System.out.println("¿Número de votos obtenidos por el partido A?");
+			numeroVotosA = sc.nextInt();
+			
+			if(numeroVotosA>mayorVotosA) {
+				mayorVotosA = numeroVotosA;
+				ProvinciaAFavorita =nombreProvincia;
+			}
+				
+			System.out.println("¿Número de votos obtenidos por el partido B?");
+			numeroVotosB = sc.nextInt();
+			if(numeroVotosB>mayorVotosB) {
+				mayorVotosB = numeroVotosB;
+				ProvinciaBFavorita =nombreProvincia;
+			}
+			
+			System.out.println("¿Número de abstenciones?");
+			abstenciones =sc.nextInt();
+			if(abstenciones>100000)altaAbstencion=true;
 		}
-		
-		
-		
-		
+		 votosTotales= numeroVotosA+numeroVotosB;
+		System.out.println("Porcentaje de votos totales: "+ (votosTotales*100)/votosTotales+abstenciones);
+		System.out.println("Porcentaje de abstenciones: "+(((votosTotales*100)/votosTotales+abstenciones)-100)+"%");
+		System.out.println("El partido A obtenido mas votos en: "+ProvinciaAFavorita);
+		System.out.println("El partido B obtenido mas votos en: "+ProvinciaBFavorita);
+		if (altaAbstencion) System.out.println("Alta abstención");
 		
 		sc.close();
 	}
